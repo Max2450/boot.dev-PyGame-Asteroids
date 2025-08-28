b055981b448c24a26caa2725f77cf7f0235ac034
Max2450 <mrg245000@gmail.com>
2025-08-28 15:10:07 -0700

    Added Player class and main is now drawing the player on the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,6 +8,7 @@ def main():
     game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids Game")
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,7 +21,7 @@ def main():
                 return
         game_clock.tick(60)
         dt = game_clock.get_time() / 1000.0  # Convert milliseconds to seconds
-
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,20 @@
+from circleshape import *
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0  # in degrees
+
+    # Calculate the vertices of the triangle representing the player
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    # Override the draw method to draw the player as a triangle
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
