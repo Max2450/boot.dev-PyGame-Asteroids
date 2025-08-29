3f22347c0600ab87247db54421850fe5e09e5379
Max2450 <mrg245000@gmail.com>
2025-08-29 10:51:45 -0700

    Added asteroid split logic, boot.dev and VSCode had two different ways to do it.

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import *
 from constants import *
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +13,15 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        if self.radius > ASTEROID_MIN_RADIUS:
+            # Create two smaller asteroids
+            for _ in range(2):
+                new_radius = self.radius / 2
+                new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+                new_speed = random.randint(40, 100)
+                new_velocity = pygame.Vector2(random.uniform(-1, 1), random.uniform(-1, 1)).normalize() * new_speed
+                new_asteroid.velocity = new_velocity
+                self.containers[0].add(new_asteroid)
+        self.kill()
--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                     break
 
